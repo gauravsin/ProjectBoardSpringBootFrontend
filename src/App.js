ae9601440e37c2a1c31b8e60e08dd5fbccc0da1a
Gaurav <gaurav.sinha1563@gmail.com>
2019-03-25 11:05:41 -0500

    Completed lecture 8/18 on Layout. jsx intro. html within javascript. Components. Class and functional components. className vs class.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProjectBoard from "./components/ProjectBoard";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="alert alert-warning">Welcome to the Project Board</h1>
+        <Navbar />
+        <ProjectBoard />
       </div>
     );
   }
