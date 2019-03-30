aba6a65982c7c148057d3e875d04539dbbc9ff51
Gaurav <gaurav.sinha1563@gmail.com>
2019-03-29 20:33:43 -0500

    Completed video 17/18. Update operations part 1. Loading the task to update to the state.

--- src/components/ProjectTask/UpdateProjectTask.js
+++ src/components/ProjectTask/UpdateProjectTask.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { getProjectTask } from "../../actions/projectTaskActions.js";
 
 class UpdateProjectTask extends Component {
+  componentDidMount() {
+    const { pt_id } = this.props.match.params;
+    this.props.getProjectTask(pt_id);
+  }
   render() {
     return (
       <div className="addProjectTask">
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
-              <a href="/ProjectBoard.html" className="btn btn-light">
+              <a href="/" className="btn btn-light">
                 Back to Board
               </a>
-              <h4 className="display-4 text-center">Add /Update Project Task</h4>
+              <h4 className="display-4 text-center">
+                Add /Update Project Task
+              </h4>
               <form>
                 <div className="form-group">
                   <input
@@ -28,14 +39,20 @@ class UpdateProjectTask extends Component {
                   />
                 </div>
                 <div className="form-group">
-                  <select className="form-control form-control-lg" name="status">
+                  <select
+                    className="form-control form-control-lg"
+                    name="status"
+                  >
                     <option value="">Select Status</option>
                     <option value="TO_DO">TO DO</option>
                     <option value="IN_PROGRESS">IN PROGRESS</option>
                     <option value="DONE">DONE</option>
                   </select>
                 </div>
-                <input type="submit" className="btn btn-primary btn-block mt-4" />
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
               </form>
             </div>
           </div>
@@ -45,4 +62,18 @@ class UpdateProjectTask extends Component {
   }
 }
 
-export default UpdateProjectTask;
+UpdateProjectTask.propTypes = {
+  project_task: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  getProjectTask: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  project_task: state.project_task,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { getProjectTask }
+)(UpdateProjectTask);
